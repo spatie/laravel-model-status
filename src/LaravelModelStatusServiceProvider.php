5500864f1fefd8bf36860c76159626742201e1e4
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-02-06 14:09:57 +0100

    Service provider CS

--- src/LaravelModelStatusServiceProvider.php
+++ src/LaravelModelStatusServiceProvider.php
@@ -11,13 +11,14 @@ class LaravelModelStatusServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             $this->loadMigrationsFrom(__DIR__.'/../database/migrations/');
         }
+
         if (! class_exists('CreateStatusesTable')) {
             $timestamp = date('Y_m_d_His', time());
 
             $this->publishes([
-                __DIR__ . '/../database/migrations/create_statuses_table.php'
-                => database_path('migrations/'.$timestamp.'create_statuses_table.php')
-                ], 'migrations');
+                __DIR__ . '/../database/migrations/create_statuses_table.php' =>
+                    database_path('migrations/'.$timestamp.'create_statuses_table.php'),
+            ], 'migrations');
         }
     }
 }
