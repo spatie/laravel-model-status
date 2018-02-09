5b42198155b9b147878ad8db78cde73996563af3
TVke <thomas_verhelst@telenet.be>
2018-02-09 15:46:27 +0100

    add scope

--- src/HasStatuses.php
+++ src/HasStatuses.php
@@ -2,6 +2,9 @@
 
 namespace Spatie\ModelStatus;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Spatie\ModelStatus\Exceptions\InvalidStatus;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
@@ -9,7 +12,7 @@ trait HasStatuses
 {
     public function statuses(): MorphMany
     {
-        return $this->morphMany(ModelStatusServiceProvider::getStatusModel(), 'model');
+        return $this->morphMany(ModelStatusServiceProvider::getStatusModel(), 'model')->latest();
     }
 
     public function status(): ?Status
@@ -17,9 +20,10 @@ trait HasStatuses
         return $this->latestStatus();
     }
 
+
     public function setStatus(string $name, string $reason = ''): self
     {
-        if (! $this->isValidStatus($name, $reason)) {
+        if (!$this->isValidStatus($name, $reason)) {
             throw InvalidStatus::create($name, $reason);
         }
 
@@ -44,9 +48,62 @@ trait HasStatuses
         $names = is_array($names) ? array_flatten($names) : func_get_args();
 
         if (count($names) < 1) {
-            return $this->statuses()->latest()->orderByDesc('id')->first();
+            return $this->statuses()->orderByDesc('id')->first();
         }
 
-        return $this->statuses()->whereIn('name', $names)->latest()->orderByDesc('id')->first();
+        return $this->statuses()->whereIn('name', $names)->orderByDesc('id')->first();
+    }
+
+//    public function scopeHasStatus(Builder $query, string $status): Builder
+//    {
+//        $modelType = str_replace("\\", '\\\\', get_class($this));
+//
+//        $tableName = $this->getTable();
+//
+//        $statusTable = 'statuses';
+//
+//
+//        $query->select($tableName.'.*');
+//
+//        $subSelect = DB::table($statusTable)
+//            ->select($statusTable.'.name')
+//            ->whereRaw($statusTable.'.model_id = '.$tableName.'.id')
+//            ->whereRaw($statusTable.".model_type = '".$modelType."'")
+//            ->orderByDesc($statusTable.'.id')
+//            ->take(1);
+//
+//        $query->selectSub($subSelect, 'current_status');
+//
+//        $query->groupBy($tableName.'.id');
+//
+//        $query->having('current_status', $status);
+//
+//        dump($query);
+//
+//        return $query;
+//    }
+
+    public function scopeHasStatus($builder, string $name)
+    {
+        return $builder->whereIn('id', function ($query) use ($name) {
+            $query
+                ->select('model_id')
+                ->from('statuses')
+                ->where('model_type', static::class)
+                ->where('name', $name)
+                ->whereIn('id', function ($query) use ($name) {
+                    $query
+                        ->select('model_id')
+                        ->from('statuses')
+                        ->where('model_type', static::class)
+                        ->latest()
+                        ->groupBy('model_id');
+                });
+        });
+    }
+
+    public function getStatusAttribute(): string
+    {
+        return $this->latestStatus();
     }
 }
