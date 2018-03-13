91d67178a17ca382d3037aeea52f4608d5c6dfe1
Vincent Mimoun-Prat <vpratfr@gmail.com>
2018-03-13 11:23:23 +0100

    Remove some comments + rename $e

--- tests/StatusEventsTest.php
+++ tests/StatusEventsTest.php
@@ -22,33 +22,27 @@ class StatusEventsTest extends TestCase
     /** @test */
     public function it_fires_an_event_when_status_changes()
     {
-        // Prepare
         $this->testModel->setStatus('pending', 'waiting on action');
         Event::fake(); // Fake after, in order not to get the initial event
 
-        // Act
         $this->testModel->setStatus('status a', 'Reason a');
 
-        // Assert
         Event::assertDispatched(StatusUpdated::class,
-            function (StatusUpdated $e) {
-                return $e->getModel()->is($this->testModel)
-                       && $e->getNewStatus() === 'status a'
-                       && $e->getOldStatus() === 'pending';
+            function (StatusUpdated $event) {
+                return $event->getModel()->is($this->testModel)
+                       && $event->getNewStatus() === 'status a'
+                       && $event->getOldStatus() === 'pending';
             });
     }
 
     /** @test */
     public function it_does_not_fire_an_event_when_status_stays_same()
     {
-        // Prepare
         $this->testModel->setStatus('pending', 'waiting on action');
         Event::fake(); // Fake after, in order not to get the initial event
 
-        // Act
         $this->testModel->setStatus('pending', 'Still waiting');
 
-        // Assert
         Event::assertNotDispatched(StatusUpdated::class);
     }
 }
